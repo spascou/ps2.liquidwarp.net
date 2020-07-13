2413b995f91820887da2be5d78d953ad23268e08
Sylvain Pascou <sylvain@pascou.net>
2020-07-13 19:13:57 +0900

    Adapt to altair stuff being moved out of ps2-analysis

--- generate/altair_utils.py
+++ generate/altair_utils.py
@@ -1,3 +1,51 @@
+from typing import List
+
+import altair
+from ps2_census.enums import Faction
+
+# Axis
+X: str = "x"
+Y: str = "y"
+
+# Faction
+_FACTION_COLOR_DOMAIN: List[int] = [
+    Faction.TERRAN_REPUBLIC.name,
+    Faction.VANU_SOVEREIGNTY.name,
+    Faction.NEW_CONGLOMERATE.name,
+    Faction.NONE.name,
+]
+_FACTION_COLOR_RANGE: List[str] = ["red", "purple", "blue", "green"]
+
+FACTION_SELECTION = altair.selection_multi(fields=["Faction"])
+FACTION_COLOR = altair.condition(
+    FACTION_SELECTION,
+    altair.Color(
+        "Faction:N",
+        scale=altair.Scale(domain=_FACTION_COLOR_DOMAIN, range=_FACTION_COLOR_RANGE),
+        legend=None,
+    ),
+    altair.value("lightgray"),
+)
+
+# Simulation point type
+_SIMULATION_POINT_TYPE_COLOR_DOMAIN: List[str] = ["cursor", "pellet"]
+_SIMULATION_POINT_TYPE_COLOR_RANGE: List[str] = ["red", "green"]
+
+SIMULATION_POINT_TYPE_SELECTION = altair.selection_multi(fields=["Type"])
+SIMULATION_POINT_TYPE_COLOR = altair.condition(
+    SIMULATION_POINT_TYPE_SELECTION,
+    altair.Color(
+        "Type:N",
+        scale=altair.Scale(
+            domain=_SIMULATION_POINT_TYPE_COLOR_DOMAIN,
+            range=_SIMULATION_POINT_TYPE_COLOR_RANGE,
+        ),
+        legend=None,
+    ),
+    altair.value("lightgray"),
+)
+
+
 def dark_theme():
 
     lightColor: str = "#fff"
--- generate/site.py
+++ generate/site.py
@@ -1,3 +1,4 @@
+import math
 import os
 import shutil
 import subprocess
@@ -5,7 +6,7 @@ from datetime import datetime, timezone
 from itertools import groupby
 from multiprocessing import Pool, cpu_count
 from pathlib import Path
-from typing import Any, Dict, Iterator, List
+from typing import Any, Dict, Iterator, List, Optional, Tuple
 
 import altair
 import altair_saver
@@ -48,9 +49,15 @@ from ps2_analysis.weapons.vehicle.generate import (
     parse_vehicle_weapon_data,
 )
 from ps2_analysis.weapons.vehicle.vehicle_weapon import VehicleWeapon
-from ps2_census.enums import Faction, ItemCategory
+from ps2_census.enums import Faction, ItemCategory, PlayerState
 
-from .altair_utils import dark_theme
+from .altair_utils import (
+    SIMULATION_POINT_TYPE_COLOR,
+    SIMULATION_POINT_TYPE_SELECTION,
+    X,
+    Y,
+    dark_theme,
+)
 from .constants import (
     CHART_TEMPLATE_PATH,
     DATA_FILES_DIRECTORY,
@@ -193,6 +200,115 @@ def generate_predefined_pages():
             )
 
 
+def magdump_simulation_chart(
+    fire_mode: FireMode,
+    shots: int,
+    runs: int = 1,
+    control_time: int = 0,
+    auto_burst_length: Optional[int] = None,
+    recentering: bool = False,
+    recentering_response_time: int = 1_000,
+    recentering_inertia_factor: float = 0.3,
+    player_state: PlayerState = PlayerState.STANDING,
+    width: Optional[int] = None,
+    height: Optional[int] = None,
+) -> altair.HConcatChart:
+
+    assert (width or height) and not (width and height)
+
+    datapoints: List[dict] = []
+
+    simulation: Iterator[Tuple[int, Tuple[float, float], List[Tuple[float, float]]]]
+    for simulation in (
+        fire_mode.simulate_shots(
+            shots=shots,
+            control_time=control_time,
+            auto_burst_length=auto_burst_length,
+            recentering=recentering,
+            recentering_response_time=recentering_response_time,
+            recentering_inertia_factor=recentering_inertia_factor,
+            player_state=player_state,
+        )
+        for _ in range(runs)
+    ):
+
+        t: int
+        cursor_coor: Tuple[float, float]
+        pellets_coors: List[Tuple[float, float]]
+        for t, cursor_coor, pellets_coors in simulation:
+
+            cursor_x, cursor_y = cursor_coor
+
+            datapoints.append({"Time": t, X: cursor_x, Y: cursor_y, "Type": "cursor"})
+
+            for pellet_x, pellet_y in pellets_coors:
+                datapoints.append(
+                    {"Time": t, X: pellet_x, Y: pellet_y, "Type": "pellet"}
+                )
+
+    min_x: float = min((d[X] for d in datapoints))
+    max_x: float = max((d[X] for d in datapoints))
+    min_y: float = min((d[Y] for d in datapoints))
+    max_y: float = max((d[Y] for d in datapoints))
+
+    if height:
+
+        if max_y != min_y:
+
+            width = int(math.ceil((max_x - min_x) * height / (max_y - min_y)))
+
+        else:
+
+            width = 0
+
+    elif width:
+
+        if max_x != min_x:
+
+            height = int(math.ceil((max_y - min_y) * width / (max_x - min_x)))
+
+        else:
+
+            height = 0
+
+    dataset: altair.Data = altair.Data(values=datapoints)
+
+    chart: altair.Chart = (
+        altair.Chart(dataset)
+        .mark_point()
+        .encode(
+            x=altair.X(
+                f"{X}:Q",
+                axis=altair.Axis(title="horizontal angle (degrees)"),
+                scale=altair.Scale(domain=(min_x, max_x), zero=False),
+            ),
+            y=altair.Y(
+                f"{Y}:Q",
+                axis=altair.Axis(title="vertical angle (degrees)"),
+                scale=altair.Scale(domain=(min_y, max_y), zero=False),
+            ),
+            color=SIMULATION_POINT_TYPE_COLOR,
+            tooltip=["Time:Q", f"{X}:Q", f"{Y}:Q"],
+        )
+        .properties(width=width, height=height)
+        .interactive()
+    )
+
+    legend: altair.Chart = (
+        altair.Chart(dataset)
+        .mark_point()
+        .encode(
+            y=altair.Y("Type:N", axis=altair.Axis(orient="right")),
+            color=SIMULATION_POINT_TYPE_COLOR,
+        )
+        .add_selection(SIMULATION_POINT_TYPE_SELECTION)
+    )
+
+    result: altair.HConcatChart = altair.hconcat(chart, legend)
+
+    return result
+
+
 def generate_dynamic_pages():
 
     generate_infantry_weapons_stats_pages()
@@ -285,7 +401,8 @@ def _generate_infantry_weapons_stats_page(
 
                     print(f"Simulating {magdump_sim_output_path}")
 
-                    magdump_chart: altair.HConcatChart = fm.altair_simulate_shots(
+                    magdump_chart: altair.HConcatChart = magdump_simulation_chart(
+                        fire_mode=fm,
                         shots=fm.max_consecutive_shots,
                         runs=50,
                         recentering=False,
